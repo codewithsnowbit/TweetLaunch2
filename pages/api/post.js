fef51e0e14fe058c814884f7819c8b470e755a8e
Dhairya Shah <dtshah24022006@gmail.com>
2022-05-09 08:40:14 +0000

    Basic commit

---
+++ pages/api/post.js
@@ -0,0 +1,20 @@
+import Twit from "twit";
+
+export default function handler(req, res){
+    const T = new Twit({
+        consumer_key: process.env.API_KEY,
+        consumer_secret: process.env.API_SECRET,
+        access_token: process.env.ACCESS_TOKEN,
+        access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    });
+    const text = "Hey!";
+    const onFinish = (err, reply) => {
+        if (err) {
+          console.log("Error: ", err.message);
+        } else {
+          console.log("Success: ", reply);
+        }
+      };
+    
+      T.post("statuses/update", { status: text }, onFinish);
+}
